493053ed4679e2e187bedf1248fba4d6905530f7
muhammadAmeen786 <mohdameeendar@gmail.com>
2022-11-16 00:50:00 +0530

    Node introduction

---
+++ node.js
@@ -0,0 +1,9 @@
+/* Node js is a runtime environment for js using which we can run the js codes in our local machine.
+It is asynchronous in nature.
+It is event driven .
+there are two types of events : System events and Custom Events.
+It was made to build the servers.
+NPM : Node package manager : some functionalities which are discoverd later are added in our programs
+through these small programmes. These packages are the pre written codes. 
+*/
+
